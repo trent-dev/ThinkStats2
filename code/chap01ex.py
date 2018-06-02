a5b16760a22ed288a0efde68412bd99c81e5d53c
Trent <devtrentoh@gmail.com>
2018-06-02 19:18:45 +0900

    Ex 01-2

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,53 @@ import sys
 import nsfg
 import thinkstats2
 
+def CleanFemResp(df):
+    pass
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+def ValidatePregnum(resp, preg):
+    respToPregMap = nsfg.MakePregMap(preg)
+    for index, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[index]
+        pregCount = len(respToPregMap[caseid])
+        if pregCount != pregnum:
+            print(caseid, pregCount, pregnum)
+            return False
+
+    return True
+
+def PrintPregNums(df):
+    print(df.pregnum.value_counts().sort_index())
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    # read and validate the respondent file
+    resp = ReadFemResp()
+
+    assert(len(resp) == 7643)
+
+    # read and validate the pregnancy file
+    preg = nsfg.ReadFemPreg()
+    print(preg.shape)
+
+    assert len(preg) == 13593
+
+    # validate that the pregnum column in `resp` matches the number
+    # of entries in `preg`
+    assert(ValidatePregnum(resp, preg))
+
+    PrintPregNums(preg)
+
     print('%s: All tests passed.' % script)
 
 
